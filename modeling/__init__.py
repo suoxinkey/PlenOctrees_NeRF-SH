2e5222b358538ae43aa8f61bab14cadd3e823998
Minye Wu <huanlekf@163.com>
2020-05-20 09:27:43 +0000

    update

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -4,5 +4,6 @@ from .rfrender import RFRender
 
 
 def build_model(cfg):
-    model = RFRender(cfg.MODEL.COARSE_RAY_SAMPLING, cfg.MODEL.FINE_RAY_SAMPLING, boarder_weight= cfg.MODEL.BOARDER_WEIGHT, sample_method = cfg.MODEL.SAMPLE_METHOD, same_space_net = cfg.MODEL.SAME_SPACENET)
+    model = RFRender(cfg.MODEL.COARSE_RAY_SAMPLING, cfg.MODEL.FINE_RAY_SAMPLING, boarder_weight= cfg.MODEL.BOARDER_WEIGHT, sample_method = cfg.MODEL.SAMPLE_METHOD, same_space_net = cfg.MODEL.SAME_SPACENET,
+                    TriKernel_include_input = cfg.MODEL.TKERNEL_INC_RAW)
     return model
